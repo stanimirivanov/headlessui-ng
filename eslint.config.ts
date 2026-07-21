61a317c2f45fc8258256f2791955501587334631
Stanimir Ivanov <stanimir.ivanov@gmail.com>
2026-07-21 19:59:02 +0200

    [stanimirivanov/headlessui-ng#1] Initiate angular project (#2)

---
+++ eslint.config.ts
@@ -0,0 +1,49 @@
+import { defineConfig } from 'eslint/config';
+import tseslint from 'typescript-eslint';
+import eslintPluginPrettierRecommended from 'eslint-plugin-prettier/recommended';
+import importX from 'eslint-plugin-import-x';
+
+export default defineConfig([
+  {
+    ignores: [
+      'node_modules/**',
+      '.vscode/**',
+      '.idea/**',
+      '**/package-lock.json',
+    ],
+  },
+  tseslint.configs.eslintRecommended,
+  ...tseslint.configs.recommended,
+  {
+    plugins: {
+      'import-x': importX,
+    },
+    languageOptions: {
+      parserOptions: {
+        projectService: true,
+        tsconfigRootDir: import.meta.dirname,
+      },
+    },
+    settings: {
+      'import-x/resolver': {
+        typescript: {
+          alwaysTryTypes: true,
+          project: ['./tsconfig.json'],
+        },
+      },
+    },
+    rules: {
+      '@typescript-eslint/no-unused-vars': [
+        'warn',
+        {
+          argsIgnorePattern: '^_',
+          varsIgnorePattern: '^_',
+          caughtErrorsIgnorePattern: '^_',
+        },
+      ],
+      '@typescript-eslint/no-explicit-any': 'warn',
+    },
+  },
+  // Automatically turns off rules that conflict with Prettier and runs Prettier as a rule
+  eslintPluginPrettierRecommended,
+]);
---
+++ projects/core/src/lib/core.ts
@@ -0,0 +1,9 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'lib-core',
+  imports: [],
+  template: ` <p>core works!</p> `,
+  styles: ``,
+})
+export class Core {}
